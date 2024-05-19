2433b0294d8d6223d38e645ea09d4adb6d55c282
Minutenreis <Minutenreis@gmail.com>
2024-05-19 15:40:48 +0200

    Implemented CDCL

---
+++ CDCL/CDCL.py
@@ -0,0 +1,136 @@
+from copy import deepcopy
+import sys
+import cnf_utils
+import time
+import resource
+
+statTimeStart = time.time()
+statUP = 0
+statDecision = 0
+statConflicts = 0
+
+
+def getAllLiterals(cnf: list[list[int]]) -> set[int]:
+    return set([abs(l) for c in cnf for l in c])
+
+# todo: optimize this method 
+def decide(allLiterals: set[int], v: tuple[list[int], list[int]], decisionLevel: int) -> set[int]:
+    global statDecision
+    statDecision += 1
+    
+    for literal in allLiterals:
+        if literal not in v[0] and -literal not in v[0]:
+            v[0].append(literal)
+            v[1].append(decisionLevel)
+            return v
+    raise Exception("All literals are assigned")
+        
+            
+def propagate(cnf: list[list[int]], v: tuple[list[int], list[int]], decisionLevel: int) -> tuple[tuple[list[int], list[int]], list[int]]:   
+    global statUP
+    i = 0
+    
+    while i < len(cnf):
+        clause = cnf[i]
+        unassignedLiteral = 0
+        numUnassigned = 0
+        for literal in clause:
+            # clause is satisfied
+            if literal in v[0]:
+                numUnassigned = -1
+                break
+            # variable is assigned
+            if -literal in v[0]:
+                continue
+            
+            unassignedLiteral = literal
+            numUnassigned += 1
+            
+        # clause is a unit clause -> assign the unassigned literal
+        if numUnassigned == 1:
+            v[0].append(unassignedLiteral)
+            v[1].append(decisionLevel)
+            i = 0
+            statUP += 1
+            continue
+        # clause is unsatisfied
+        elif numUnassigned == 0:
+            global statConflicts
+            statConflicts += 1
+            return v, clause
+        
+        i += 1
+    return v, None
+
+def analyzeConflict(cnf: list[list[int]], v: tuple[list[int], list[int]], c_conflict: list[int]) -> tuple[list[int], int]:
+    # get all decision literals in conflict clause
+    decisionLiterals = []
+    decisionLevel = 1
+    for i in range(len(v[1])):
+        if v[1][i] >= decisionLevel:
+            decisionLevel += 1
+            decisionLiterals.append(v[0][i])
+    
+    # negate all decision literals
+    c_learned = [-l for l in decisionLiterals]
+    
+    # -2 because at the end of the loop decisionLevel will be 1 higher than in the clause and we want to go back 1
+    return c_learned, decisionLevel - 2
+
+# todo implement
+def applyRestartPolicy(cnf: list[list[int]], v: tuple[list[int], list[int]], decisionLevel: int, ogCnf: list[list[int]]):
+    return cnf, v, decisionLevel
+
+def backtrack(v : tuple[list[int], list[int]], new_decision_level: int) -> tuple[list[int], list[int]]:
+    for i, literal in enumerate(v[1]):
+        if literal > new_decision_level:
+            return (v[0][:i], v[1][:i])
+    else:
+        return ([],[])
+            
+    
+
+# if sat, return True and v, else return False and list of added clauses
+def CDCL(cnf: list[list[int]]) -> tuple[bool, list[int] | list[list[int]]] :
+    ogCnf = deepcopy(cnf)
+    decisionLevel = 0
+    allLiterals = getAllLiterals(cnf)
+    v : tuple[list[int], list[int]] = ([],[])
+    while len(v[0]) < len(allLiterals):
+        decisionLevel += 1
+        v = decide(allLiterals, v, decisionLevel)
+        v, c_conflict = propagate(cnf, v, decisionLevel)
+        while c_conflict is not None:
+            if decisionLevel == 0:
+                return False, cnf[len(ogCnf):]
+            c_learned, decisionLevel = analyzeConflict(cnf, v, c_conflict)
+            cnf.append(c_learned)
+            v = backtrack(v, decisionLevel)
+            v, c_conflict = propagate(cnf, v, decisionLevel)
+        cnf,v,decisionLevel = applyRestartPolicy(cnf, v, decisionLevel, ogCnf)
+    return True, v[0]
+            
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python CDCL.py filename")
+
+filename = sys.argv[1]
+
+cnf = cnf_utils.read_cnf(filename)
+sat, v = CDCL(cnf)
+
+if not sat:
+    with open("proof.drat", "w") as f:
+        for clause in v:
+            f.write(" ".join(map(str, clause)) + " 0"+ "\n")
+
+statTimeEnd = time.time()
+statPeakMemoryMB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
+
+cnf_utils.fancy_output("CDCL Solver", sat, v, filename, [
+    ("unit propagations", str(statUP)),
+    ("decisions", str(statDecision)),
+    ("conflicts", str(statConflicts)),
+    ("peak memory", str(statPeakMemoryMB)+" MB (assumes Ubuntu)"),
+    ("time", str(statTimeEnd - statTimeStart)+" s")
+])
--- benchmark.py
+++ benchmark.py
@@ -24,28 +24,33 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     if iteration == total: 
         print()
 
-if len(sys.argv) != 4:
-    print("Usage: python test.py <solver> <n> <tries>")
+if len(sys.argv) != 5:
+    print("Usage: python test.py <solver1> <solver2> <n> <tries>")
     print("<solver>: path to the solver executable")
     sys.exit(1)
 
-solver = sys.argv[1]
-proof = False
-if (solver.upper() == "CDCL"):
-    solver = "CDCL/CDCL.py"
-    proof = True
-elif (solver.upper() == "DPLL"):
-    solver = "DPLL/DPLL.py"
-elif (solver.upper() == "DP"):
-    solver = "DP/DP.py"
+def getPath(solver):
+    if (solver.upper() == "CDCL"):
+        return "CDCL/CDCL.py"
+    elif (solver.upper() == "DPLL"):
+        return "DPLL/DPLL.py"
+    elif (solver.upper() == "DP"):
+        return "DP/DP.py"
+    else:
+        return solver
 
-n = sys.argv[2]
-tries = int(sys.argv[3])
+solver1 = sys.argv[1]
+solver1Path = getPath(solver1)
+    
+solver2 = sys.argv[2]
+solver2Path = getPath(solver2)
 
-statTimeCadical = 0
-statTimeSolver = 0
+n = sys.argv[3]
+tries = int(sys.argv[4])
+
+statTimeSolver1 = 0
+statTimeSolver2 = 0
 statTimeGen = 0
-statTimeDrat = 0
 
 printProgressBar(0,tries, prefix = 'Progress:', suffix = 'Complete', length = 50)
 for i in range(tries):
@@ -53,36 +58,31 @@ for i in range(tries):
     subprocess.call(["python3", "RandomCNF/randomCnf.py", n, str(round(3.8 * int(n))), "3"])
     timeGenEnd = time.perf_counter()
     
-    timeSolverStart = time.perf_counter()
-    satSolver = subprocess.call(["python3",solver, "randomCnf.cnf"],stdout=subprocess.DEVNULL)
-    timeSolverEnd = time.perf_counter()
+    timeSolver1Start = time.perf_counter()
+    satSolver1 = subprocess.call(["python3",solver1Path, "randomCnf.cnf"],stdout=subprocess.DEVNULL)
+    timeSolver1End = time.perf_counter()
     
-    if (proof and satSolver == 20):
-        timeDratStart = time.perf_counter()
-        subprocess.call(["./Submodules/drat-trim/drat-trim", "proof.drat", "randomCnf.cnf"], stdout=subprocess.DEVNULL)
-        timeDratEnd = time.perf_counter()
-        statTimeDrat += timeDratEnd - timeDratStart
-    else:
-        timeCadicalStart = time.perf_counter()
-        satCadical = subprocess.call(["./Submodules/cadical/build/cadical", "randomCnf.cnf"], stdout=subprocess.DEVNULL)
-        timeCadicalEnd = time.perf_counter()
-        statTimeCadical += timeCadicalEnd - timeCadicalStart
-        if (satSolver != satCadical):
-            print()
-            print("Error: Solver output does not match Cadical output")
-            print("Cadical: ", satCadical)
-            print("Solver: ", satSolver)
-            sys.exit(1)
-
-    statTimeSolver += timeSolverEnd - timeSolverStart
+    timeSolver2Start = time.perf_counter()
+    satSolver2 = subprocess.call(["python3",solver2Path, "randomCnf.cnf"],stdout=subprocess.DEVNULL)
+    timeSolver2End = time.perf_counter()
+    
+    if (satSolver1 != satSolver2):
+        print("Different results")
+        sys.exit(1)
+    
+    statTimeSolver1 += timeSolver1End - timeSolver1Start
+    statTimeSolver2 += timeSolver2End - timeSolver2Start
     statTimeGen += timeGenEnd - timeGenStart
     
     
     printProgressBar(i+1,tries, prefix = 'Progress:', suffix = 'Complete', length = 50)
-print("All tests passed")
-print("Time spent in drat-trim: ", statTimeDrat, "s")
-print("Time spent in Cadical: ", statTimeCadical, "s")
-print("Time spent in Solver: ", statTimeSolver, "s")
+
+print(f"Time spent in {solver1}: ", statTimeSolver1, "s")
+print(f"Time spent in {solver2}: ", statTimeSolver2, "s")
 print("Time spent generating CNFs: ", statTimeGen, "s")
+if statTimeSolver1 < statTimeSolver2:
+    print(f"{solver1} was {round((statTimeSolver2 / statTimeSolver1 - 1) * 100)}% faster than {solver2}")
+else:
+    print(f"{solver2} was {round((statTimeSolver1 / statTimeSolver2 - 1) * 100)}% faster than {solver1}")
     
 
